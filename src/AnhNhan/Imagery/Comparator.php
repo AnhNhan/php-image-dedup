443ef2445a3abcf9873ba69b1656026c23ed7487
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-09-19 04:30:54 +0200

    Adding true deviation to image comparator

--- src/AnhNhan/Imagery/Comparator.php
+++ src/AnhNhan/Imagery/Comparator.php
@@ -9,15 +9,42 @@ namespace AnhNhan\Imagery;
  */
 class Comparator
 {
-    public static function compareByPixels(Image $img1, Image $img2, $deviation = 0.015)
+    private static function rgbColor(Image $img, $x, $y)
+    {
+        $colorIndex = imagecolorat($img->getImageData(), $x, $y);
+        $rgb = imagecolorsforindex($img->getImageData(), $colorIndex);
+
+        return [
+            'r' => $rgb['red'],
+            'g' => $rgb['green'],
+            'b' => $rgb['blue'],
+            'a' => $rgb['alpha'],
+        ];
+    }
+
+    private static function rgbCompare(array $color1, array $color2, $deviation = 3)
+    {
+        foreach ($color1 as $key => $value) {
+            $delta = $color1[$key] - $color2[$key];
+            if ($delta < 0) {
+                $delta *= -1;
+            }
+
+            if ($value - $delta < $value - $deviation || $value + $delta > $value + $deviation) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static function compareByPixels(Image $img1, Image $img2, $deviation = 3)
     {
         $img1Width = $img1->getImageWidth();
         $img1Height = $img1->getImageHeight();
-        $img1Data = $img1->getImageData();
 
         $img2Width = $img2->getImageWidth();
         $img2Height = $img2->getImageHeight();
-        $img2Data = $img2->getImageData();
 
         if ($img1Width != $img2Width || $img1Height != $img2Height) {
             throw new \Exception('Sizes do not match!');
@@ -25,10 +52,10 @@ class Comparator
 
         for ($i_y = 0; $i_y < $img1Height; $i_y++) {
             for ($i_x = 0; $i_x < $img1Width; $i_x++) {
-                $img1Color = imagecolorat($img1Data, $i_x, $i_y);
-                $img2Color = imagecolorat($img2Data, $i_x, $i_y);
+                $img1Color = self::rgbColor($img1, $i_x, $i_y);
+                $img2Color = self::rgbColor($img2, $i_x, $i_y);
 
-                if ($img1Color != $img2Color) {
+                if (!self::rgbCompare($img1Color, $img2Color, $deviation)) {
                     return false;
                 }
             }
