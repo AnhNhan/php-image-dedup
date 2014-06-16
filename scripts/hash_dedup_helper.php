909f6dd33e76f33c297abf715c0e4da70c1097eb
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-06-16 21:14:27 +0200

    Cleaning up hash_dedup_helper, a few stuff done better.

--- scripts/hash_dedup_helper.php
+++ scripts/hash_dedup_helper.php
@@ -3,7 +3,8 @@
 include_once __DIR__ . '/../vendor/autoload.php';
 include_once __DIR__ . '/../src/root.php';
 
-use \YamwLibs\Libs\Cli;
+use YamwLibs\Libs\Cli\Color;
+use YamwLibs\Libs\Cli\Cli;
 
 if ($argc > 1) {
     // Do something
@@ -11,14 +12,19 @@ if ($argc > 1) {
     $path = sdx($argv);
 
 
-    $fileList = scanInDirectory($path, '\.(jpg|png)', false);
-    Cli\Cli::notice(sprintf('Scanning %s ... (%d files)', $path, count($fileList)));
+    $fileList = scanInDirectory($path, '\.(jpg|png)', true);
+    Cli::notice(sprintf('Scanning %s ... (%d files)', $path, count($fileList)));
     $hashList = [];
 
     foreach ($fileList as $file) {
+        $hash = @hash_file('sha256', $file);
+        if (!$hash) {
+            Cli::error("Could not process file '$file'!");
+            continue;
+        }
         $hashList[] = [
             'name' => $file,
-            'hash' => sha1_file($file) . md5_file($file) . crc32(file_get_contents($file)),
+            'hash' => $hash,
         ];
     }
 
@@ -33,8 +39,8 @@ if ($argc > 1) {
         }
     }
 
-    Cli\Cli::success('We have ' . Cli\Color::green(count($duplicateList)) .
-        ' duplicate groups, with ' . Cli\Color::yellow($count) . ' duplicates');
+    Cli::success('We have ' . Color::green(count($duplicateList)) .
+        ' duplicate groups, with ' . Color::yellow($count) . ' duplicates');
 
     /*
     foreach ($duplicateList as $duplicate) {
@@ -53,14 +59,15 @@ if ($argc > 1) {
         $i = 1;
         foreach ($duplicates as $entry) {
             $oldName = $entry['name'];
-            $newName = preg_replace('/\.(.*?)/', '_' . str_pad($i, 3, '0', STR_PAD_LEFT) . '.$1', $first['name']);
-            Cli\Cli::notice(sprintf('Renaming %s to %s', Cli\Color::yellow($oldName), Cli\Color::green($newName)));
+            $newName = preg_replace('/\.(jpg|png)$/', '_' . str_pad($i, 3, '0', STR_PAD_LEFT) . '.$1', $first['name']);
+            Cli::notice(sprintf('Renaming %s to %s', Color::yellow($oldName), Color::green($newName)));
             rename($oldName, $newName);
+            ++$i;
         }
 
         println();
     }
 } else {
     // Print non-existing help
-    Cli\Cli::notice('No input. $1 must be a path!');
+    Cli::notice('No input. $1 must be a path!');
 }
